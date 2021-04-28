88bd575f89e29996d41b4a047778c5f3e25b3382
Kitsunetic <shenhaichenhai@gmail.com>
2021-04-28 13:34:02 +0900

    도둑질

--- programmers.co.kr/DP/도둑질/main.py
+++ programmers.co.kr/DP/도둑질/main.py
@@ -14,13 +14,14 @@ def solution(money):
         dp[2 + j][j] = money[j] + money[2 + j]
         mv = max(mv, money[j] + money[2 + j])
 
-    for i in range(3, L - 1):
+    for i in range(3, L - 2):
         for j in range(t):
-            if not (0 <= i + j < L):
-                break
-
             v = max(dp[i + j - 2][j], dp[i + j - 3][j]) + money[i + j]
             dp[i + j][j] = v
             mv = max(mv, v)
+    for j in range(2):
+        v = max(dp[L - 2 + j - 2][j], dp[L - 2 + j - 3][j]) + money[L - 2 + j]
+        dp[L - 2 + j][j] = v
+        mv = max(mv, v)
 
-    return mv, dp
+    return mv
